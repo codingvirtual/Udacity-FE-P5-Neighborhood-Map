428696c0c19b9754771a3a94cc192a181c2a2cf3
Greg Palen <codingvirtual@gmail.com>
2015-06-04 16:41:10 -0500

    read locations from locations.json using XHTTP request.

--- js/app.js
+++ js/app.js
@@ -1,39 +1,35 @@
-var map;    // declares a global map variable
+var map = {};       // declares a global map variable
+var markers = [];   // Array of map markers
 
-// need a marker object
-// return location
-// return reviews
-// return something else
-var MapObject = {
+function createMapMarker(marker) {
+    // marker is an object with additional data about the pin for a single location
+    return new google.maps.Marker({
+        position: new google.maps.LatLng(marker.lat, marker.long),
+        title: marker.name,
+        map: map
+    });
+}
 
-    "id": 0,
-    "name": "name",
-    "category": "category",
-    "lat": 0,
-    "long": 0,
-    "description": "description",
-    "createMapMarker": function () {
+// infoWindows are the little helper windows that open when you click
+// or hover over a pin on a map. They usually contain more information
+// about a location.
+function createInfoWindow(marker) {
+    return new google.maps.InfoWindow({
+        content: marker.description
+    });
+}
 
-        // marker is an object with additional data about the pin for a single location
-        return new google.maps.Marker({
-            position: this.lat + ',' + this.long,
-            title: this.name
-        });
-    },
-
-
-    // infoWindows are the little helper windows that open when you click
-    // or hover over a pin on a map. They usually contain more information
-    // about a location.
-    "infoWindow": function () {
-        return new google.maps.InfoWindow({
-            content: this.description
+function displayMarkers() {
+    for (var element in markers) {
+        var MapObject = markers[element];
+        MapObject.marker = createMapMarker(MapObject);
+        MapObject.infoWindow = createInfoWindow(MapObject);
+        google.maps.event.addListener(MapObject.marker, 'click', function () {
+            MapObject.infoWindow.open(map, MapObject.marker);
         });
     }
-};
+}
 
-// Array of map markers
-var markers = [];
 
 // Sets up a new map centered on the Las Vegas Strip
 function initializeMap() {
@@ -53,36 +49,36 @@ function initializeMap() {
     /*
      callback(results, status) makes sure the search returned results for a location.
      If so, it creates a new map marker for that location.
-     */
-    function callback(results, status) {
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-            createMapMarker(results[0]);
-        }
-    }
 
+     function callback(results, status) {
+     if (status == google.maps.places.PlacesServiceStatus.OK) {
+     createMapMarker(results[0]);
+     }
+     }
+     */
     /*
      pinPoster(locations) takes in the array of locations created by locationFinder()
      and fires off Google place searches for each location
      */
-    function pinPoster(locations) {
-
-        // creates a Google place search service object. PlacesService does the work of
-        // actually searching for location data.
-        var service = new google.maps.places.PlacesService(map);
-
-        // Iterates through the array of locations, creates a search object for each location
-        for (var place in locations) {
-
-            // the search request object
-            var request = {
-                query: locations[place]
-            };
-
-            // Actually searches the Google Maps API for location data and runs the callback
-            // function with the search results after each search.
-            service.textSearch(request, callback);
-        }
-    }
+    //function pinPoster(locations) {
+    //
+    //    // creates a Google place search service object. PlacesService does the work of
+    //    // actually searching for location data.
+    //    var service = new google.maps.places.PlacesService(map);
+    //
+    //    // Iterates through the array of locations, creates a search object for each location
+    //    for (var place in locations) {
+    //
+    //        // the search request object
+    //        var request = {
+    //            query: locations[place]
+    //        };
+    //
+    //        // Actually searches the Google Maps API for location data and runs the callback
+    //        // function with the search results after each search.
+    //        service.textSearch(request, callback);
+    //    }
+    //}
 
 // locations is an array of location strings returned from locationFinder()
 //    locations = locationFinder();
@@ -91,7 +87,28 @@ function initializeMap() {
 // the locations array
 //    pinPoster(locations);
 
+
+    var xmlhttp = new XMLHttpRequest();
+    var url = "js/data/locations.json";
+
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            responseFunction(xmlhttp.responseText);
+        }
+    };
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+
+    function responseFunction(response) {
+        var arr = JSON.parse(response);
+        for (var element in arr) {
+            markers.push(arr[element]);
+        }
+        displayMarkers();
+    }
+
 }
 
 // Calls the initializeMap() function when the page loads
-window.addEventListener('load', initializeMap);
+google.maps.event.addDomListener(window, 'load', initializeMap);
