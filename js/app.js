958be5738dfb0ad81d4910f6466e2c30afbd3123
Greg Palen <codingvirtual@gmail.com>
2015-06-04 21:25:14 -0500

    add Yelp calls

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
 var map = {};       // declares a global map variable
-var markers = [];   // Array of map markers
+function ViewModel (markers) {
+    var self = this;
+    self.markers = ko.observableArray(markers);
+    self.toggleMarker = function(marker) {
+        switch (this.marker.isVisible) {
+            case true:
+                this.marker.setMap(null);
+                break;
+            case false:
+                this.marker.setMap(map);
+                break;
+        }
+        this.marker.isVisible = !(this.marker.isVisible);
+    }
+
+}
 
 function createMapMarker(marker) {
     // marker is an object with additional data about the pin for a single location
     return new google.maps.Marker({
         position: new google.maps.LatLng(marker.lat, marker.long),
         title: marker.name,
-        map: map
     });
 }
 
@@ -15,14 +29,20 @@ function createMapMarker(marker) {
 // about a location.
 function createInfoWindow(marker) {
     return new google.maps.InfoWindow({
-        content: marker.description
+        content: buildInfoContent(marker)
     });
 }
 
-function displayMarkers() {
+function buildInfoContent(marker) {
+    return "<h3>" + marker.name + "</h3>" + marker.description;
+
+}
+
+function populateMarkerObjects(markers) {
     for (var element in markers) {
         var MapObject = markers[element];
         MapObject.marker = createMapMarker(MapObject);
+        MapObject.marker.isVisible = false;
         MapObject.infoWindow = createInfoWindow(MapObject);
         google.maps.event.addListener(MapObject.marker, 'click', function () {
             MapObject.infoWindow.open(map, MapObject.marker);
@@ -30,9 +50,67 @@ function displayMarkers() {
     }
 }
 
+function yelp() {
+    var auth = {
+        //
+        // Update with your auth tokens.
+        //
+        consumerKey : "vD-WJpgPbOpyvhEDMpt7PA",
+        consumerSecret : "qbgNN0ibK48h7XhixYpce9YKVbA",
+        accessToken : "81N4jR3e5lxR3lM0C7DzGcU08xdDk2pd",
+        // This example is a proof of concept, for how to use the Yelp v2 API with javascript.
+        // You wouldn't actually want to expose your access token secret like this in a real application.
+        accessTokenSecret : "tMUB7DAXmwJQoJ97GPur7R3setY",
+        serviceProvider : {
+            signatureMethod : "HMAC-SHA1"
+        }
+    };
+
+    var terms = 'Margaritaville';
+    var near = 'Las+Vegas';
+
+    var accessor = {
+        consumerSecret : auth.consumerSecret,
+        tokenSecret : auth.accessTokenSecret
+    };
+    var parameters = [];
+    parameters.push(['term', terms]);
+    parameters.push(['location', near]);
+    parameters.push(['callback', 'cb']);
+    parameters.push(['oauth_consumer_key', auth.consumerKey]);
+    parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+    parameters.push(['oauth_token', auth.accessToken]);
+    parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
+
+    var message = {
+        'action' : 'http://api.yelp.com/v2/search',
+        'method' : 'GET',
+        'parameters' : parameters
+    };
+
+    OAuth.setTimestampAndNonce(message);
+    OAuth.SignatureMethod.sign(message, accessor);
+
+    var parameterMap = OAuth.getParameterMap(message.parameters);
+    console.log(parameterMap);
+
+    $.ajax({
+        'url' : message.action,
+        'data' : parameterMap,
+        'dataType' : 'jsonp',
+        'jsonpCallback' : 'cb',
+        'success' : function(data, textStats, XMLHttpRequest) {
+            console.log(data);
+            //$("body").append(output);
+        }
+    });
+
+}
 
 // Sets up a new map centered on the Las Vegas Strip
 function initializeMap() {
+
+    var markers = [];
 
     var mapOptions = {
         center: {lat: 36.113, lng: -115.172},
@@ -105,7 +183,8 @@ function initializeMap() {
         for (var element in arr) {
             markers.push(arr[element]);
         }
-        displayMarkers();
+        populateMarkerObjects(markers);
+        ko.applyBindings(new ViewModel(markers));
     }
 
 }
